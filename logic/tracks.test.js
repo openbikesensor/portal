8f5237a182bb9246de28eaba88c2ed3a4b9ca650
Paul Bienkowski <pb@opatut.de>
2020-11-17 18:24:13 +0100

    test: add test data from postman-test to test `addPointsToTrack`

--- logic/tracks.test.js
+++ logic/tracks.test.js
@@ -1,7 +1,29 @@
 var { addPointsToTrack } = require('./tracks');
+var TrackInfo = require('./TrackInfo');
+
+var { test1 } = require('./_tracks_testdata');
 
 describe('addPointsToTrack', () => {
   it('is a function', () => {
     expect(typeof addPointsToTrack).toBe('function');
+  });
+
+  it('works on the sample data with an empty track', () => {
+    const trackInfo = new TrackInfo({}, { points: [] });
+    addPointsToTrack(trackInfo, test1);
+    var points = trackInfo.trackData.points;
+    expect(points).toHaveLength(324);
+    expect(points[0]).toEqual({
+      date: '12.07.2020',
+      time: '09:02:59',
+      latitude: 0,
+      longitude: 0,
+      course: 0,
+      speed: 0,
+      d1: '255',
+      d2: '255',
+      flag: '0',
+      private: '0',
+    });
   });
 });
