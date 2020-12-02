5770229945c8c1423f870fc4f8de7df633757fa3
Paul Bienkowski <pb@opatut.de>
2020-12-02 20:55:04 +0100

    feat: add track length and duration (seconds) to parsed information

--- src/models/TrackData.js
+++ src/models/TrackData.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
+const turf = require('turf');
 
 const schema = new mongoose.Schema(
   {
     slug: { type: String, lowercase: true, unique: true },
     numEvents: { type: Number, default: 0 },
     recordedAt: { type: Date },
+    recordedUntil: { type: Date },
+    trackLength: { type: Number },
     points: [
       {
         date: String,
@@ -33,6 +36,8 @@ schema.pre('validate', function (next) {
   next();
 });
 
+schema.set('toJSON', { virtuals: true });
+
 class TrackData extends mongoose.Model {
   slugify() {
     this.slug = 'td-' + String((Math.random() * Math.pow(36, 6)) | 0).toString(36);
@@ -42,14 +47,16 @@ class TrackData extends mongoose.Model {
     return this.points.filter((p) => p.flag).length;
   }
 
-  getRecoredAt() {
-    const firstPointWithDate = this.points.find((p) => p.date && p.time);
-    if (!firstPointWithDate) {
+  getRecoredAt(findEnd = false) {
+    const pointsWithDate = this.points.filter((p) => p.date && p.time);
+
+    if (!pointsWithDate.length) {
       return null;
     }
 
-    const [day, month, year] = firstPointWithDate.date.split('.');
-    const combinedString = `${year}-${month}-${day} ${firstPointWithDate.time}.000+2000`;
+    const point = pointsWithDate[findEnd ? pointsWithDate.length - 1 : 0];
+    const [day, month, year] = point.date.split('.');
+    const combinedString = `${year}-${month}-${day} ${point.time}.000+2000`;
     const parsedDate = new Date(combinedString);
     if (isNaN(parsedDate.getDate())) {
       return null;
@@ -63,7 +70,53 @@ class TrackData extends mongoose.Model {
     trackData.points = points;
     trackData.numEvents = trackData.countEvents();
     trackData.recordedAt = trackData.getRecoredAt();
+    trackData.recordedUntil = trackData.getRecoredAt(true);
+    trackData.trackLength = trackData.measureTrackLength();
     return trackData;
+  }
+
+  measureTrackLength() {
+    let totalLength = 0;
+    for (const [a, b] of pairwise(map(this.points, (p) => turf.point([p.longitude, p.latitude])))) {
+      const legLengthMeters = turf.distance(a, b) * 1000;
+      totalLength += legLengthMeters;
+    }
+    return totalLength;
+  }
+
+  get duration() {
+    if (this.recordedAt == null || this.recordedUntil == null) {
+      return null;
+    }
+
+    return (this.recordedUntil.getTime() - this.recordedAt.getTime()) / 1000;
+  }
+}
+
+function* pairwise(iter) {
+  let last;
+  let firstLoop = true;
+  for (const it of iter) {
+    if (firstLoop) {
+      firstLoop = false;
+    } else {
+      yield [last, it];
+    }
+    last = it;
+  }
+}
+
+function* enumerate(iter) {
+  let i = 0;
+  for (const it of iter) {
+    yield [i, it];
+    i++;
+  }
+}
+
+function* map(iter, fn) {
+  for (const [i, it] of enumerate(iter)) {
+    yield fn(it, i);
   }
 }
 
