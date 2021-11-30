989b2947918b60cf37eec3006acee9d94109da69
Paul Bienkowski <pb@opatut.de>
2021-11-30 19:54:18 +0100

    remove debug logs

--- frontend/src/components/LoginButton.js
+++ frontend/src/components/LoginButton.js
@@ -7,11 +7,9 @@ export default function LoginButton(props)  {
   const [busy, setBusy] = React.useState(false)
 
   const onClick = React.useCallback(async (e) => {
-    console.log('aa')
     e.preventDefault()
     setBusy(true)
     const url = await api.makeLoginUrl()
-    console.log('go', url)
     window.location.href = url
     setBusy(false)
   }, [setBusy])
