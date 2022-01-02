0f816e168025c14d71ba186d8b9aa8fa33e30f73
Dennis Boldt <info@dennis-boldt.de>
2022-01-02 21:03:16 +0100

    Fix env-var names in osm2pgsql.sh

--- osm2pgsql.sh
+++ osm2pgsql.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-osm2pgsql --create --hstore --style tools/roads_import.lua -O flex -d postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST/$POSTGRES_DB /pbf/*.osm.pbf
+osm2pgsql --create --hstore --style tools/roads_import.lua -O flex -d postgresql://$OBS_POSTGRES_USER:$OBS_POSTGRES_PASSWORD@$OBS_POSTGRES_HOST/$OBS_POSTGRES_DB /pbf/*.osm.pbf
 
