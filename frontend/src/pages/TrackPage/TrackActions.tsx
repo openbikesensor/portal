2a9e3549b5d45717c38bdcc43a994cb826dde576
Paul Bienkowski <pb@opatut.de>
2022-03-02 19:48:02 +0100

    Fix track download dropdown appearing for non-authors, and update tooltip

--- frontend/src/pages/TrackPage/TrackActions.tsx
+++ frontend/src/pages/TrackPage/TrackActions.tsx
@@ -5,39 +5,36 @@ import {Icon, Popup, Button, Dropdown} from 'semantic-ui-react'
 export default function TrackActions({slug, isAuthor, onDownload}) {
   return (
     <>
-      {isAuthor ? (
-        <Dropdown text="Download" button upward>
-          <Dropdown.Menu>
-            <Popup
-              content={
-                <>
-                  <p>Only you, the author of this track, can download the original file.</p>
-                  <p>
-                    This is the file as it was uploaded to the server, without modifications, and it can be used with
-                    other tools. Exporting to other formats, and downloading modified files, will be implemented soon.
-                  </p>
-                </>
-              }
-                trigger={<Dropdown.Item text="Original" onClick={() => onDownload('original.csv')} />}
-            />
-                  <Dropdown.Item text="Track (GPX)" onClick={() => onDownload('track.gpx')} />
-          </Dropdown.Menu>
-        </Dropdown>
-      ) : (
-        <>
-          <Button disabled>Download</Button>
-          <Popup
-            content={<p>Only the author of this track can download the original file.</p>}
-            trigger={<Icon name="info circle" />}
-          />
-        </>
-      )}
-
       {isAuthor && (
         <Link to={`/tracks/${slug}/edit`}>
           <Button primary>Edit track</Button>
         </Link>
       )}
+
+      <Dropdown text="Download" button upward>
+        <Dropdown.Menu>
+          <Dropdown.Item text="Original" onClick={() => onDownload('original.csv')} disabled={!isAuthor} />
+          <Dropdown.Item text="Track (GPX)" onClick={() => onDownload('track.gpx')} />
+        </Dropdown.Menu>
+      </Dropdown>
+
+      <Popup
+        trigger={<Icon name="info circle" />}
+        offset={[12, 0]}
+        content={
+          isAuthor ? (
+            <>
+              <p>Only you, the author of this track, can download the original file.</p>
+              <p>
+                This is the file as it was uploaded to the server, without modifications, and it can be used with other
+                tools.
+              </p>
+            </>
+          ) : (
+            <p>Only the author of this track can download the original file.</p>
+          )
+        }
+      />
     </>
   )
 }
