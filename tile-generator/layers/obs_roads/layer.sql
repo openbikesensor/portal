36fd8c492c4402bac20938f4354885e0ee397371
Paul Bienkowski <pb@opatut.de>
2022-03-16 22:20:15 +0100

    Fix split roads for usage_count

--- tile-generator/layers/obs_roads/layer.sql
+++ tile-generator/layers/obs_roads/layer.sql
@@ -1,3 +1,5 @@
+DROP FUNCTION IF EXISTS layer_obs_roads(geometry, int);
+
 CREATE OR REPLACE FUNCTION layer_obs_roads(bbox geometry, zoom_level int)
 RETURNS TABLE(
   way_id bigint,
@@ -22,7 +24,8 @@ RETURNS TABLE(
       PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY distance_overtaker) as distance_overtaker_median,
       array_agg(distance_overtaker) as distance_overtaker_array,
       count(overtaking_event.id)::int as distance_overtaker_count,
-      (select count(id) from road_usage where road_usage.way_id = road.way_id) as usage_count,
+      (select count(id) from road_usage where road_usage.way_id = road.way_id and
+          (road.directionality != 0 or road_usage.direction_reversed = r.rev)) as usage_count,
       r.dir as direction,
       case when road.directionality = 0 then r.dir else 0 end as offset_direction
     FROM road
@@ -30,6 +33,6 @@ RETURNS TABLE(
     FULL OUTER JOIN overtaking_event ON (road.way_id = overtaking_event.way_id and (road.directionality != 0 or overtaking_event.direction_reversed = r.rev))
     -- WHERE road.name = 'Merzhauser Straße'
     WHERE road.geometry && bbox
-    GROUP BY road.name, road.way_id, road.geometry, road.directionality, r.dir;
+    GROUP BY road.name, road.way_id, road.geometry, road.directionality, r.dir, r.rev;
 
 $$ LANGUAGE SQL IMMUTABLE;
