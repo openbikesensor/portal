248f8b4a6fd8d232f7e5a22fd4e91ef14b46da09
Paul Bienkowski <pb@opatut.de>
2022-07-24 18:43:26 +0200

    Translate MapPage

--- frontend/src/pages/MapPage/LayerSidebar.tsx
+++ frontend/src/pages/MapPage/LayerSidebar.tsx
@@ -1,53 +1,66 @@
-import React from 'react'
-import _ from 'lodash'
-import {connect} from 'react-redux'
-import {List, Select, Input, Divider, Label, Checkbox, Header} from 'semantic-ui-react'
+import React from "react";
+import _ from "lodash";
+import { connect } from "react-redux";
+import {
+  List,
+  Select,
+  Input,
+  Divider,
+  Label,
+  Checkbox,
+  Header,
+} from "semantic-ui-react";
+import { useTranslation } from "react-i18next";
 
 import {
   MapConfig,
   setMapConfigFlag as setMapConfigFlagAction,
   initialState as defaultMapConfig,
-} from 'reducers/mapConfig'
-import {colorByDistance, colorByCount, viridisSimpleHtml} from 'mapstyles'
-import {ColorMapLegend, DiscreteColorMapLegend} from 'components'
+} from "reducers/mapConfig";
+import { colorByDistance, colorByCount, viridisSimpleHtml } from "mapstyles";
+import { ColorMapLegend, DiscreteColorMapLegend } from "components";
 
-const BASEMAP_STYLE_OPTIONS = [
-  {value: 'positron', key: 'positron', text: 'Positron'},
-  {value: 'bright', key: 'bright', text: 'OSM Bright'},
-]
+const BASEMAP_STYLE_OPTIONS = ["positron", "bright"];
 
 const ROAD_ATTRIBUTE_OPTIONS = [
-  {value: 'distance_overtaker_mean', key: 'distance_overtaker_mean', text: 'Overtaker distance mean'},
-  {value: 'distance_overtaker_min', key: 'distance_overtaker_min', text: 'Overtaker distance minimum'},
-  {value: 'distance_overtaker_max', key: 'distance_overtaker_max', text: 'Overtaker distance maximum'},
-  {value: 'distance_overtaker_median', key: 'distance_overtaker_median', text: 'Overtaker distance median'},
-  {value: 'overtaking_event_count', key: 'overtaking_event_count', text: 'Event count'},
-  {value: 'usage_count', key: 'usage_count', text: 'Usage count'},
-  {value: 'zone', key: 'zone', text: 'Overtaking distance zone'}
-]
+  "distance_overtaker_mean",
+  "distance_overtaker_min",
+  "distance_overtaker_max",
+  "distance_overtaker_median",
+  "overtaking_event_count",
+  "usage_count",
+  "zone",
+];
 
 function LayerSidebar({
   mapConfig,
   setMapConfigFlag,
 }: {
-  mapConfig: MapConfig
-  setMapConfigFlag: (flag: string, value: unknown) => void
+  mapConfig: MapConfig;
+  setMapConfigFlag: (flag: string, value: unknown) => void;
 }) {
+  const { t } = useTranslation();
   const {
-    baseMap: {style},
-    obsRoads: {show: showRoads, showUntagged, attribute, maxCount},
-    obsEvents: {show: showEvents},
-  } = mapConfig
+    baseMap: { style },
+    obsRoads: { show: showRoads, showUntagged, attribute, maxCount },
+    obsEvents: { show: showEvents },
+  } = mapConfig;
 
   return (
     <div>
       <List relaxed>
         <List.Item>
-          <List.Header>Basemap Style</List.Header>
+          <List.Header>{t("MapPage.sidebar.baseMap.style.label")}</List.Header>
           <Select
-            options={BASEMAP_STYLE_OPTIONS}
+            options={BASEMAP_STYLE_OPTIONS.map((value) => ({
+              value,
+              key: value,
+              text: t(`MapPage.sidebar.baseMap.style.${value}`),
+            }))}
             value={style}
-            onChange={(_e, {value}) => setMapConfigFlag('baseMap.style', value)}
+            onChange={(_e, { value }) =>
+              setMapConfigFlag("baseMap.style", value)
+            }
           />
         </List.Item>
         <Divider />
@@ -56,12 +69,12 @@ function LayerSidebar({
             toggle
             size="small"
             id="obsRoads.show"
-            style={{float: 'right'}}
+            style={{ float: "right" }}
             checked={showRoads}
-            onChange={() => setMapConfigFlag('obsRoads.show', !showRoads)}
+            onChange={() => setMapConfigFlag("obsRoads.show", !showRoads)}
           />
           <label htmlFor="obsRoads.show">
-            <Header as="h4">Road segments</Header>
+            <Header as="h4">{t("MapPage.sidebar.obsRoads.title")}</Header>
           </label>
         </List.Item>
         {showRoads && (
@@ -69,49 +82,80 @@ function LayerSidebar({
             <List.Item>
               <Checkbox
                 checked={showUntagged}
-                onChange={() => setMapConfigFlag('obsRoads.showUntagged', !showUntagged)}
-                label="Include roads without data"
+                onChange={() =>
+                  setMapConfigFlag("obsRoads.showUntagged", !showUntagged)
+                }
+                label={t("MapPage.sidebar.obsRoads.showUntagged.label")}
               />
             </List.Item>
             <List.Item>
-              <List.Header>Color based on</List.Header>
+              <List.Header>
+                {t("MapPage.sidebar.obsRoads.attribute.label")}
+              </List.Header>
               <Select
                 fluid
-                options={ROAD_ATTRIBUTE_OPTIONS}
+                options={ROAD_ATTRIBUTE_OPTIONS.map((value) => ({
+                  value,
+                  key: value,
+                  text: t(`MapPage.sidebar.obsRoads.attribute.${value}`),
+                }))}
                 value={attribute}
-                onChange={(_e, {value}) => setMapConfigFlag('obsRoads.attribute', value)}
+                onChange={(_e, { value }) =>
+                  setMapConfigFlag("obsRoads.attribute", value)
+                }
               />
             </List.Item>
-            {attribute.endsWith('_count') ? (
-             <>
+            {attribute.endsWith("_count") ? (
+              <>
+                <List.Item>
+                  <List.Header>
+                    {t("MapPage.sidebar.obsRoads.maxCount.label")}
+                  </List.Header>
+                  <Input
+                    fluid
+                    type="number"
+                    value={maxCount}
+                    onChange={(_e, { value }) =>
+                      setMapConfigFlag("obsRoads.maxCount", value)
+                    }
+                  />
+                </List.Item>
+                <List.Item>
+                  <ColorMapLegend
+                    map={_.chunk(
+                      colorByCount(
+                        "obsRoads.maxCount",
+                        mapConfig.obsRoads.maxCount,
+                        viridisSimpleHtml
+                      ).slice(3),
+                      2
+                    )}
+                    twoTicks
+                  />
+                </List.Item>
+              </>
+            ) : (
               <List.Item>
-                <List.Header>Maximum value</List.Header>
-                <Input
-                  fluid
-                  type="number"
-                  value={maxCount}
-                  onChange={(_e, {value}) => setMapConfigFlag('obsRoads.maxCount', value)}
+                <DiscreteColorMapLegend
+                  map={colorByDistance("distance_overtaker")[3].slice(2)}
                 />
               </List.Item>
-              <List.Item>
-                <ColorMapLegend map={_.chunk(colorByCount('obsRoads.maxCount', mapConfig.obsRoads.maxCount, viridisSimpleHtml ).slice(3), 2)} twoTicks />
-              </List.Item></>
             ) :
             attribute.endsWith('zone') ? (
              <>
               <List.Item>
-                <Label size="small" style={{background: "blue",color:"white"}}>urban (1.5&nbsp;m)</Label>
-                <Label size="small" style={{background: "cyan", color:"black"}}>rural (2&nbsp;m)</Label>
+                <Label size="small" style={{background: "blue",color:"white"}}>{t("general.urban")} (1.5&nbsp;m)</Label>
+                <Label size="small" style={{background: "cyan", color:"black"}}>{t("general.rural")}(2&nbsp;m)</Label>
               </List.Item></>
             ) :
             (
               <>
                 <List.Item>
-                  <List.Header>Urban</List.Header>
+                  <List.Header>{_.startCase(t("general.urban"))}</List.Header>
                   <DiscreteColorMapLegend map={colorByDistance('distance_overtaker')[3][5].slice(2)} />
                 </List.Item>
                 <List.Item>
-                  <List.Header>Rural</List.Header>
+                  <List.Header>{_.startCase(t("general.rural"))}</List.Header>
                   <DiscreteColorMapLegend map={colorByDistance('distance_overtaker')[3][3].slice(2)} />
                 </List.Item>
               </>
@@ -124,29 +168,29 @@ function LayerSidebar({
             toggle
             size="small"
             id="obsEvents.show"
-            style={{float: 'right'}}
+            style={{ float: "right" }}
             checked={showEvents}
-            onChange={() => setMapConfigFlag('obsEvents.show', !showEvents)}
+            onChange={() => setMapConfigFlag("obsEvents.show", !showEvents)}
           />
           <label htmlFor="obsEvents.show">
-            <Header as="h4">Event points</Header>
+            <Header as="h4">{t("MapPage.sidebar.obsEvents.title")}</Header>
           </label>
         </List.Item>
         {showEvents && (
           <>
             <List.Item>
-              <List.Header>Urban</List.Header>
-                <DiscreteColorMapLegend map={colorByDistance('distance_overtaker')[3][5].slice(2)} />
-              </List.Item>
+              <List.Header>{_.startCase(t('general.urban'))}</List.Header>
+              <DiscreteColorMapLegend map={colorByDistance('distance_overtaker')[3][5].slice(2)} />
+            </List.Item>
             <List.Item>
-              <List.Header>Rural</List.Header>
-                <DiscreteColorMapLegend map={colorByDistance('distance_overtaker')[3][3].slice(2)} />
+              <List.Header>{_.startCase(t('general.rural'))}</List.Header>
+              <DiscreteColorMapLegend map={colorByDistance('distance_overtaker')[3][3].slice(2)} />
             </List.Item>
           </>
         )}
       </List>
     </div>
-  )
+  );
 }
 
 export default connect(
@@ -158,6 +202,6 @@ export default connect(
       //
     ),
   }),
-  {setMapConfigFlag: setMapConfigFlagAction}
+  { setMapConfigFlag: setMapConfigFlagAction }
   //
-)(LayerSidebar)
+)(LayerSidebar);
