e081851923dd16f9db75fb7dbc6b472af4e492b9
Paul Bienkowski <pb@opatut.de>
2022-07-24 18:43:26 +0200

    Translate MapPage

--- frontend/src/pages/MapPage/RoadInfo.tsx
+++ frontend/src/pages/MapPage/RoadInfo.tsx
@@ -1,83 +1,95 @@
-import React, {useState, useCallback} from 'react'
-import _ from 'lodash'
-import {Segment, Menu, Header, Label, Icon, Table} from 'semantic-ui-react'
-import {Layer, Source} from 'react-map-gl'
-import {of, from, concat} from 'rxjs'
-import {useObservable} from 'rxjs-hooks'
-import {switchMap, distinctUntilChanged} from 'rxjs/operators'
-import {Chart} from 'components'
-import {pairwise} from 'utils'
+import React, { useState, useCallback } from "react";
+import _ from "lodash";
+import { Segment, Menu, Header, Label, Icon, Table } from "semantic-ui-react";
+import { Layer, Source } from "react-map-gl";
+import { of, from, concat } from "rxjs";
+import { useObservable } from "rxjs-hooks";
+import { switchMap, distinctUntilChanged } from "rxjs/operators";
+import { Chart } from "components";
+import { pairwise } from "utils";
+import { useTranslation } from "react-i18next";
 
 import api from 'api'
 import {colorByDistance, borderByZone} from 'mapstyles'
 
-import styles from './styles.module.less'
+import styles from "./styles.module.less";
 
 function selectFromColorMap(colormap, value) {
-  let last = null
+  let last = null;
   for (let i = 0; i < colormap.length; i += 2) {
     if (colormap[i + 1] > value) {
-      return colormap[i]
+      return colormap[i];
     }
   }
-  return colormap[colormap.length - 1]
+  return colormap[colormap.length - 1];
 }
 
-const UNITS = {distanceOvertaker: 'm', distanceStationary: 'm', speed: 'km/h'}
-const LABELS = {
-  distanceOvertaker: 'Left',
-  distanceStationary: 'Right',
-  speed: 'Speed',
-  count: 'No. of Measurements',
-  min: 'Minimum',
-  median: 'Median',
-  max: 'Maximum',
-  mean: 'Average',
-}
-const ZONE_COLORS = {urban: 'blue', rural: 'cyan', motorway: 'purple'}
-const CARDINAL_DIRECTIONS = ['north', 'north-east', 'east', 'south-east', 'south', 'south-west', 'west', 'north-west']
-const getCardinalDirection = (bearing) =>
-  bearing == null
-    ? 'unknown'
-    : CARDINAL_DIRECTIONS[
-        Math.floor(((bearing / 360.0) * CARDINAL_DIRECTIONS.length + 0.5) % CARDINAL_DIRECTIONS.length)
-      ] + ' bound'
+const UNITS = {
+  distanceOvertaker: "m",
+  distanceStationary: "m",
+  speed: "km/h",
+};
+const ZONE_COLORS = { urban: "blue", rural: "cyan", motorway: "purple" };
+const CARDINAL_DIRECTIONS = [
+  "north",
+  "northEast",
+  "east",
+  "southEast",
+  "south",
+  "southWest",
+  "west",
+  "northWest",
+];
+const getCardinalDirection = (t, bearing) => {
+  if (bearing == null) {
+    return t("MapPage.roadInfo.cardinalDirections.unknown");
+  } else {
+    const n = CARDINAL_DIRECTIONS.length;
+    const i = Math.floor(((bearing / 360.0) * n + 0.5) % n);
+    const name = CARDINAL_DIRECTIONS[i];
+    return t(`MapPage.roadInfo.cardinalDirections.${name}`);
+  }
+};
 
-function RoadStatsTable({data}) {
+function RoadStatsTable({ data }) {
+  const { t } = useTranslation();
   return (
     <Table size="small" compact>
       <Table.Header>
         <Table.Row>
           <Table.HeaderCell textAlign="right"></Table.HeaderCell>
-          {['distanceOvertaker', 'distanceStationary', 'speed'].map((prop) => (
+          {["distanceOvertaker", "distanceStationary", "speed"].map((prop) => (
             <Table.HeaderCell key={prop} textAlign="right">
-              {LABELS[prop]}
+              {t(`MapPage.roadInfo.${prop}`)}
             </Table.HeaderCell>
           ))}
         </Table.Row>
       </Table.Header>
       <Table.Body>
-        {['count', 'min', 'median', 'max', 'mean'].map((stat) => (
+        {["count", "min", "median", "max", "mean"].map((stat) => (
           <Table.Row key={stat}>
-            <Table.Cell>{LABELS[stat]}</Table.Cell>
-            {['distanceOvertaker', 'distanceStationary', 'speed'].map((prop) => (
-              <Table.Cell key={prop} textAlign="right">
-                {(data[prop]?.statistics?.[stat] * (prop === `speed` && stat != 'count' ? 3.6 : 1)).toFixed(
-                  stat === 'count' ? 0 : 2
-                )}
-                {stat !== 'count' && ` ${UNITS[prop]}`}
-              </Table.Cell>
-            ))}
+            <Table.Cell> {t(`MapPage.roadInfo.${stat}`)}</Table.Cell>
+            {["distanceOvertaker", "distanceStationary", "speed"].map(
+              (prop) => (
+                <Table.Cell key={prop} textAlign="right">
+                  {(
+                    data[prop]?.statistics?.[stat] *
+                    (prop === `speed` && stat != "count" ? 3.6 : 1)
+                  ).toFixed(stat === "count" ? 0 : 2)}
+                  {stat !== "count" && ` ${UNITS[prop]}`}
+                </Table.Cell>
+              )
+            )}
           </Table.Row>
         ))}
       </Table.Body>
     </Table>
-  )
+  );
 }
 
-function HistogramChart({bins, counts, zone}) {
+function HistogramChart({ bins, counts, zone }) {
   const diff = bins[1] - bins[0]
-  const colortype = zone=="rural" ? 3:5;
+  const colortype = zone === "rural" ? 3 : 5;
   const data = _.zip(
     bins.slice(0, bins.length - 1).map((v) => v + diff / 2),
     counts
@@ -88,19 +100,19 @@ function HistogramChart({bins, counts, zone}) {
 
   return (
     <Chart
-      style={{height: 240}}
+      style={{ height: 240 }}
       option={{
-        grid: {top: 30, bottom: 30, right: 30, left: 30},
+        grid: { top: 30, bottom: 30, right: 30, left: 30 },
         xAxis: {
-          type: 'value',
-          axisLabel: {formatter: (v) => `${Math.round(v * 100)} cm`},
+          type: "value",
+          axisLabel: { formatter: (v) => `${Math.round(v * 100)} cm` },
           min: 0,
           max: 2.5,
         },
         yAxis: {},
         series: [
           {
-            type: 'bar',
+            type: "bar",
             data,
 
             barMaxWidth: 20,
@@ -108,20 +120,21 @@ function HistogramChart({bins, counts, zone}) {
         ],
       }}
     />
-  )
+  );
 }
 
-export default function RoadInfo({clickLocation}) {
-  const [direction, setDirection] = useState('forwards')
+export default function RoadInfo({ clickLocation }) {
+  const { t } = useTranslation();
+  const [direction, setDirection] = useState("forwards");
 
   const onClickDirection = useCallback(
-    (e, {name}) => {
-      e.preventDefault()
-      e.stopPropagation()
-      setDirection(name)
+    (e, { name }) => {
+      e.preventDefault();
+      e.stopPropagation();
+      setDirection(name);
     },
     [setDirection]
-  )
+  );
 
   const info = useObservable(
     (_$, inputs$) =>
@@ -132,7 +145,7 @@ export default function RoadInfo({clickLocation}) {
             ? concat(
                 of(null),
                 from(
-                  api.get('/mapdetails/road', {
+                  api.get("/mapdetails/road", {
                     query: {
                       ...location,
                       radius: 100,
@@ -145,43 +158,60 @@ export default function RoadInfo({clickLocation}) {
       ),
     null,
     [clickLocation]
-  )
+  );
 
   if (!clickLocation) {
-    return null
+    return null;
   }
 
-  const loading = info == null
+  const loading = info == null;
 
-  const offsetDirection = info?.road?.oneway ? 0 : direction === 'forwards' ? 1 : -1 // TODO: change based on left-hand/right-hand traffic
+  const offsetDirection = info?.road?.oneway
+    ? 0
+    : direction === "forwards"
+    ? 1
+    : -1; // TODO: change based on left-hand/right-hand traffic
 
   const content =
     !loading && !info.road ? (
-      'No road found.'
+      "No road found."
     ) : (
       <>
-        <Header as="h3">{loading ? '...' : info?.road.name || 'Unnamed way'}</Header>
+        <Header as="h3">
+          {loading
+            ? "..."
+            : info?.road.name || t("MapPage.roadInfo.unnamedWay")}
+        </Header>
 
         {info?.road.zone && (
           <Label size="small" color={ZONE_COLORS[info?.road.zone]}>
-            {info?.road.zone}
+            {t(`MapPage.roadInfo.zone.${info.road.zone}`)}
           </Label>
         )}
 
         {info?.road.oneway && (
           <Label size="small" color="blue">
-            <Icon name="long arrow alternate right" fitted /> oneway
+            <Icon name="long arrow alternate right" fitted />{" "}
+            {t("MapPage.roadInfo.oneway")}
           </Label>
         )}
 
         {info?.road.oneway ? null : (
           <Menu size="tiny" fluid secondary>
-            <Menu.Item header>Direction</Menu.Item>
-            <Menu.Item name="forwards" active={direction === 'forwards'} onClick={onClickDirection}>
-              {getCardinalDirection(info?.forwards?.bearing)}
+            <Menu.Item header>{t("MapPage.roadInfo.direction")}</Menu.Item>
+            <Menu.Item
+              name="forwards"
+              active={direction === "forwards"}
+              onClick={onClickDirection}
+            >
+              {getCardinalDirection(t, info?.forwards?.bearing)}
             </Menu.Item>
-            <Menu.Item name="backwards" active={direction === 'backwards'} onClick={onClickDirection}>
-              {getCardinalDirection(info?.backwards?.bearing)}
+            <Menu.Item
+              name="backwards"
+              active={direction === "backwards"}
+              onClick={onClickDirection}
+            >
+              {getCardinalDirection(t, info?.backwards?.bearing)}
             </Menu.Item>
           </Menu>
         )}
@@ -190,12 +220,16 @@ export default function RoadInfo({clickLocation}) {
 
         {info?.[direction]?.distanceOvertaker?.histogram && (
           <>
-            <Header as="h5">Overtaker distance distribution</Header>
-            <HistogramChart {...info[direction]?.distanceOvertaker?.histogram} />
+              <Header as="h5">
+                {t("MapPage.roadInfo.overtakerDistanceDistribution")}
+              </Header>
+            <HistogramChart
+              {...info[direction]?.distanceOvertaker?.histogram}
+            />
           </>
         )}
       </>
-    )
+    );
 
   return (
     <>
@@ -205,14 +239,22 @@ export default function RoadInfo({clickLocation}) {
             id="route"
             type="line"
             paint={{
-              'line-width': ['interpolate', ['linear'], ['zoom'], 14, 6, 17, 12],
-              'line-color': '#18FFFF',
-              'line-opacity': 0.5,
+              "line-width": [
+                "interpolate",
+                ["linear"],
+                ["zoom"],
+                14,
+                6,
+                17,
+                12,
+              ],
+              "line-color": "#18FFFF",
+              "line-opacity": 0.5,
               ...{
-                'line-offset': [
-                  'interpolate',
-                  ['exponential', 1.5],
-                  ['zoom'],
+                "line-offset": [
+                  "interpolate",
+                  ["exponential", 1.5],
+                  ["zoom"],
                   12,
                   offsetDirection,
                   19,
@@ -230,5 +272,5 @@ export default function RoadInfo({clickLocation}) {
         </div>
       )}
     </>
-  )
+  );
 }
